34d82564eeafada9933b1b34b9602e03488b06af
Alexey Malov <malov.aleksey@gmail.com>
2022-03-02 02:54:07 +0300

    Добавлены примеры по OpenGL

--- labs/03/samples/04_scissors_test/main.cpp
+++ labs/03/samples/04_scissors_test/main.cpp
@@ -1,6 +1,3 @@
-// 01_hello_triangle.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <GLFW/glfw3.h>
 #include <stdexcept>
 
@@ -74,12 +71,18 @@ public:
 private:
 	void Draw(int width, int height) override
 	{
+		glDisable(GL_SCISSOR_TEST);
 		glClearColor(0.0, 0.0, 0.0, 1.0);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glEnable(GL_SCISSOR_TEST);
+		glScissor(width / 6, height / 6, width * 4 / 6, height * 4 / 6);
+		glClearColor(0.0, 1.0, 1.0, 1.0);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glViewport(0, 0, width, height);
 
-		glBegin(GL_QUADS);
+		glBegin(GL_TRIANGLES);
 		glColor3f(1.0f, 0.0f, 0.0f);
 		glVertex2f(-0.8f, -0.8f);
 
@@ -87,10 +90,7 @@ private:
 		glVertex2f(0.8f, -0.8f);
 
 		glColor3f(0.0f, 0.0f, 1.0f);
-		glVertex2f(0.8f, 0.8f);
-
-		glColor3f(1.0f, 1.0f, 0.0f);
-		glVertex2f(-0.8f, 0.8f);
+		glVertex2f(0.0f, 0.8f);
 		glEnd();
 	}
 };
---
+++ labs/03/samples/05_alpha_test/main.cpp
@@ -0,0 +1,100 @@
+#include <GLFW/glfw3.h>
+#include <stdexcept>
+
+class GLFWInitializer final
+{
+public:
+	GLFWInitializer()
+	{
+		if (!glfwInit())
+		{
+			throw std::runtime_error("Failed to initialize GLFW");
+		}
+	}
+
+	GLFWInitializer(const GLFWInitializer&) = delete;
+	GLFWInitializer& operator=(const GLFWInitializer&) = delete;
+
+	~GLFWInitializer()
+	{
+		glfwTerminate();
+	}
+};
+
+class BaseWindow
+{
+public:
+	BaseWindow(int w, int h, const char* title)
+		: m_window{ CreateWindow(w, h, title) }
+	{
+		if (!m_window)
+		{
+			throw std::runtime_error("Failed to create window");
+		}
+	}
+	BaseWindow(const BaseWindow&) = delete;
+	BaseWindow& operator=(const BaseWindow&) = delete;
+
+	virtual ~BaseWindow()
+	{
+		glfwDestroyWindow(m_window);
+	}
+
+	void Run()
+	{
+		glfwMakeContextCurrent(m_window);
+		while (!glfwWindowShouldClose(m_window))
+		{
+			int w, h;
+			glfwGetFramebufferSize(m_window, &w, &h);
+			Draw(w, h);
+			glfwSwapBuffers(m_window);
+			glfwPollEvents();
+		}
+	}
+
+private:
+	virtual void Draw(int width, int height) = 0;
+
+	static GLFWwindow* CreateWindow(int w, int h, const char* title)
+	{
+		return glfwCreateWindow(w, h, title, nullptr, nullptr);
+	}
+	GLFWwindow* m_window;
+};
+
+class Window : public BaseWindow
+{
+public:
+	using BaseWindow::BaseWindow;
+
+private:
+	void Draw(int width, int height) override
+	{
+		glClearColor(0.0, 0.0, 0.0, 1.0);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glViewport(0, 0, width, height);
+		
+		glEnable(GL_ALPHA_TEST);
+		glAlphaFunc(GL_LEQUAL, 0.5f);
+
+		glBegin(GL_TRIANGLES);
+		glColor4f(1.0f, 0.0f, 0.0f, 0.0f);
+		glVertex2f(-0.8f, -0.8f);
+
+		glColor4f(0.0f, 1.0f, 0.0f, 1.0f);
+		glVertex2f(0.8f, -0.8f);
+
+		glColor4f(0.0f, 0.0f, 1.0f, 1.0f);
+		glVertex2f(0.0f, 0.8f);
+		glEnd();
+	}
+};
+
+int main()
+{
+	GLFWInitializer initGLFW;
+	Window window{ 800, 600, "Hello, triangle" };
+	window.Run();
+}
