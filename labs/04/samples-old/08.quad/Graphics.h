428651453bbf2c1df34639ed6483dac4363e602d
Sannikov Kirill <kir.sannikow@mail.ru>
2023-05-21 20:38:24 +0300

    Портировал 08.quad

---
+++ labs/04/samples-old/08.quad/Graphics.h
@@ -0,0 +1,23 @@
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_GRAPHICS_H__4EF982B4_DE4E_4BCF_8CD2_080AA3646DF4__INCLUDED_)
+#define AFX_GRAPHICS_H__4EF982B4_DE4E_4BCF_8CD2_080AA3646DF4__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+// Инициализация ресурсов, связанных с OpenGL
+bool InitOpenGL(HWND hWnd, BYTE colorBits = 32, BYTE depthBits = 16, BYTE stencilBits = 0);
+
+// освобождаем ресурсы, связанные с OpenGL
+void DestroyOpenGL();
+
+
+void ClearBuffers(float r, float g, float b, float a);
+
+void EndDrawing();
+
+
+#endif // !defined(AFX_GRAPHICS_H__4EF982B4_DE4E_4BCF_8CD2_080AA3646DF4__INCLUDED_)
