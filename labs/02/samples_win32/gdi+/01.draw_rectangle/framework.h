9f8fab9b30d094eaf47e9837bdc5266bcbf67d94
Sannikov Kirill <kir.sannikow@mail.ru>
2023-05-28 13:15:11 +0300

    Убрал ненужные targetver.h файлы

--- labs/02/samples_win32/gdi+/01.draw_rectangle/framework.h
+++ labs/02/samples_win32/gdi+/01.draw_rectangle/framework.h
@@ -4,7 +4,6 @@
 
 #pragma once
 
-#include "targetver.h"
 #define WIN32_LEAN_AND_MEAN             // Исключите редко используемые компоненты из заголовков Windows
 // Файлы заголовков Windows
 #include <windows.h>
