d88b4d37ea505157cdd3e1aa11bcadc1fc052aa4
Makarova Marina <marina25032002@mail.ru>
2023-05-29 15:34:45 +0300

    Портировала проект 02.HelloWorld

---
+++ labs/01/samples_win32/02.HelloWorld/02.HelloWorld.cpp
@@ -0,0 +1,11 @@
+﻿// 02.HelloWorld.cpp : Определяет точку входа для приложения.
+//
+
+#include "framework.h"
+#include "02.HelloWorld.h"
+
+int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+{
+	MessageBox(NULL, L"Hello World", L"Hi", MB_OK | MB_ICONINFORMATION);
+	return 0;
+}
