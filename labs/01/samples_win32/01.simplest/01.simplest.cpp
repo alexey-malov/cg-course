f3d5532b673b7e71ed7a33a9cd394e539dc89572
Makarova Marina <marina25032002@mail.ru>
2023-05-29 15:33:26 +0300

    Портировала проект 01.simplest

---
+++ labs/01/samples_win32/01.simplest/01.simplest.cpp
@@ -0,0 +1,11 @@
+﻿// 01.simplest.cpp : Определяет точку входа для приложения.
+//
+
+#include "framework.h"
+#include "01.simplest.h"
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
+{
+	//	возвращаемся в операционную систему
+	return 0;
+}
