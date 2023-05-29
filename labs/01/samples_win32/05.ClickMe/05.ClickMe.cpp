b8391aa6f82c98fabdde637f023fc16b0154cf16
Makarova Marina <marina25032002@mail.ru>
2023-05-29 15:41:03 +0300

    Портировала проект 05.ClickMe

---
+++ labs/01/samples_win32/05.ClickMe/05.ClickMe.cpp
@@ -0,0 +1,104 @@
+﻿// 05.ClickMe.cpp : Определяет точку входа для приложения.
+//
+
+#include "framework.h"
+#include "05.ClickMe.h"
+
+static const TCHAR MAIN_WND_CLASS_NAME[] = TEXT("My Super window class");
+static const TCHAR MAIN_WND_NAME[] = TEXT("Click me or Double click me, please!!!");
+
+LRESULT CALLBACK MainWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+BOOL RegisterMainWindowClass(HINSTANCE hInstance);
+HWND CreateMainWindow(HINSTANCE hInstance);
+int MessageLoop();
+
+int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+{
+	if (!RegisterMainWindowClass(hInstance))
+	{
+		return -1;
+	}
+
+	HWND hMainWindow = CreateMainWindow(hInstance);
+	if (!hMainWindow)
+	{
+		return -1;
+	}
+
+	ShowWindow(hMainWindow, nCmdShow);
+
+	int result = MessageLoop();
+
+	return result;
+}
+
+int MessageLoop()
+{
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
+
+HWND CreateMainWindow(HINSTANCE hInstance)
+{
+	return CreateWindowEx(
+		0,
+		MAIN_WND_CLASS_NAME,
+		MAIN_WND_NAME,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+}
+
+BOOL RegisterMainWindowClass(HINSTANCE hInstance)
+{
+	WNDCLASSEX wc;
+
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = CS_DBLCLKS; // ! Our window will be able to accept double clicks
+	wc.lpfnWndProc = &MainWndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = NULL;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = MAIN_WND_CLASS_NAME;
+	wc.hIconSm = NULL;
+
+	return RegisterClassEx(&wc) != 0;
+}
+
+LRESULT CALLBACK MainWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_LBUTTONDOWN:
+		SetWindowText(hWnd, L"Left mouse button pressed");
+		return 0; //	If an application processes this message, it should return zero.
+	case WM_LBUTTONUP:
+		SetWindowText(hWnd, L"Left mouse button released");
+		MessageBeep(-1);
+		return 0; //	If an application processes this message, it should return zero.
+	case WM_LBUTTONDBLCLK: // to receive this message our window must have CS_DBLCLKS style
+		SetWindowText(hWnd, L"Left mouse double click");
+		MessageBox(hWnd, L"Don't make me hurt!!!", L"Ouch!", MB_OK | MB_ICONEXCLAMATION);
+		return 0; //	If an application processes this message, it should return zero.
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
+
