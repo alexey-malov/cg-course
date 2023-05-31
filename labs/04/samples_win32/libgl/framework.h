3a4b18a8f61f2eb13e38905f6daea951ecd308e3
Sannikov Kirill <kir.sannikow@mail.ru>
2023-05-31 21:35:11 +0300

    Портировал 12.first_3d и вынес повторяющийся код в библиотеку libgl

---
+++ labs/04/samples_win32/libgl/framework.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Исключите редко используемые компоненты из заголовков Windows
+
+// Файлы заголовков Windows
+#include <windows.h>
+// Файлы заголовков среды выполнения C
+//#include <stdlib.h>
+//#include <malloc.h>
+//#include <memory.h>
+//#include <tchar.h>
+#include <gl\gl.h>
