a5074d10b0499af7ecffdbf20f240db29e1833ba
Sannikov Kirill <kir.sannikow@mail.ru>
2023-06-01 14:54:23 +0300

    Портировал 15.lights_and_materials

---
+++ labs/04/samples_win32/15.lights_and_materials/Resource.h
@@ -0,0 +1,41 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by gl.rc
+//
+#define IDC_MYICON                      2
+#define IDD_GL_DIALOG                   102
+#define IDD_ABOUTBOX                    103
+#define IDS_APP_TITLE                   103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDS_HELLO                       106
+#define IDI_GL                          107
+#define IDI_SMALL                       108
+#define IDC_GL                          109
+#define IDR_MAINFRAME                   128
+#define IDR_MAIN_MENU                   129
+#define ID_MATERIAL0                    32771
+#define ID_LIGHT0                       32772
+#define ID_WHITE_BACKGROUND             32773
+#define ID_BLACK_BACKGROUND             32774
+#define ID_LIGHT1                       32775
+#define ID_MATERIAL1                    32776
+#define ID_WIREFRAME                    32777
+#define ID_FILL_MODE                    32778
+#define ID_POINTS_MODE                  32779
+#define ID_MATERIAL2                    32780
+#define ID_LIGHT2                       32781
+#define ID_LIGHT3                       32782
+#define ID_MATERIAL3                    32783
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32784
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
