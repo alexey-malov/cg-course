d63e34cece8756855687b213aa69afe5ab5466ec
Makarova Marina <marina25032002@mail.ru>
2023-06-01 23:22:03 +0300

    Портировала проект 01_initialization

---
+++ labs/06/samples_win32/01_initialization/01_initialization.cpp
@@ -0,0 +1,38 @@
+﻿// 01_initialization.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <stdio.h>
+#include <gl/glew.h>
+#include <GL/glut.h>
+#include <iostream>
+
+void PrintShaderSupportInfo();
+
+int main(int argc, char* argv[])
+{
+	// initialize glut
+	glutInit(&argc, argv);
+
+	// create window
+	glutCreateWindow("Vertex shader initialization");
+
+	// initialize glew library
+	glewInit();
+
+	PrintShaderSupportInfo();
+
+	return 0;
+}
+
+void PrintShaderSupportInfo()
+{
+	// check if OpenGL implementation supports GLSL Shaders
+	if (GLEW_ARB_shader_objects)
+	{
+		std::cout << "GLSL shader objects are supported\n";
+	}
+	else
+	{
+		std::cout << "GLSL shader objects are not supported\n";
+	}
+}
