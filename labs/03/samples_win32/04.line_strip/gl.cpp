ff66a7569d8982d23d28618335412d78d3895f98
Alexey Malov <malov.aleksey@gmail.com>
2025-03-04 08:18:56 +0300

    Обновил лекции и примеры

--- labs/03/samples_win32/04.line_strip/gl.cpp
+++ labs/03/samples_win32/04.line_strip/gl.cpp
@@ -1,69 +1,73 @@
-﻿// gl.cpp : Defines the entry point for the application.
+﻿#include "framework.h"
 //
-
-#include "framework.h"
 #include "gl.h"
 #include "../libgl/libgl.h"
 
 // Global Variables:
-HINSTANCE hInst;                                               // current instance
-const TCHAR WINDOW_CLASS_NAME[] = TEXT("gl");                  // window class name
-const TCHAR WINDOW_TITLE[] = TEXT("Sine curves with GL_LINE_STRIP");     // The title bar text
+HINSTANCE hInst; // current instance
+const TCHAR WINDOW_CLASS_NAME[] = TEXT("gl"); // window class name
+const TCHAR WINDOW_TITLE[] = TEXT("Sine curves with GL_LINE_STRIP"); // The title bar text
 
 // Foward declarations of functions included in this code module:
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+ATOM MyRegisterClass(HINSTANCE hInstance);
+HWND InitInstance(HINSTANCE, int);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-    _In_opt_ HINSTANCE hPrevInstance,
-    _In_ LPWSTR    lpCmdLine,
-    _In_ int       nCmdShow)
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR lpCmdLine,
+	_In_ int nCmdShow)
 {
-    // TODO: Place code here.
-    MSG msg;
+	// Initialize global strings
+	MyRegisterClass(hInstance);
 
-    // Initialize global strings
-    MyRegisterClass(hInstance);
+	// Perform application initialization:
+	HWND windowHandle = InitInstance(hInstance, nCmdShow);
+	if (!windowHandle)
+	{
+		return FALSE;
+	}
 
-    // Perform application initialization:
-    if (!InitInstance(hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
+	// Main message loop:
+	MSG msg = {};
+	while (WM_QUIT != msg.message)
+	{
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else
+		{
+			InvalidateRect(windowHandle, nullptr, TRUE);
+		}
+	}
 
-    // Main message loop:
-    while (GetMessage(&msg, NULL, 0, 0))
-    {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
-
-    return (int) msg.wParam;
+	return (int)msg.wParam;
 }
 
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
-    WNDCLASSEXW wcex{};
+	WNDCLASSEXW wcex{};
 
-    wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.cbSize = sizeof(WNDCLASSEX);
 
-    wcex.style = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc = WndProc;
-    wcex.cbClsExtra = 0;
-    wcex.cbWndExtra = 0;
-    wcex.hInstance = hInstance;
-    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_GL));
-    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-    wcex.lpszMenuName = NULL;
-    wcex.lpszClassName = WINDOW_CLASS_NAME;
-    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_GL));
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = WINDOW_CLASS_NAME;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
-    return RegisterClassExW(&wcex);
+	return RegisterClassExW(&wcex);
 }
 
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+HWND InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
 	HWND hWnd;
 
@@ -82,13 +86,13 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 
 	if (!hWnd)
 	{
-		return FALSE;
+		return nullptr;
 	}
 
 	ShowWindow(hWnd, nCmdShow);
 	UpdateWindow(hWnd);
 
-	return TRUE;
+	return hWnd;
 }
 
 ULONGLONG g_lastTick = GetTickCount64();
@@ -151,12 +155,9 @@ void DrawScene()
 
 void OnPaint(HWND hWnd)
 {
-	PAINTSTRUCT ps;
-	BeginPaint(hWnd, &ps);
+	ValidateRect(hWnd, nullptr);
 
 	DrawScene();
-
-	EndPaint(hWnd, &ps);
 }
 
 bool OnCreate(HWND hWnd)
@@ -185,9 +186,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		{
 			return -1;
 		}
-		break;
-	case WM_ERASEBKGND:
-		return 1; // делаем вид, что очистили фон окна
 		break;
 	case WM_SIZE:
 		OnSize(hWnd);
